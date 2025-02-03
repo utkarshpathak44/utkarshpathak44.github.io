693c25b43a113bd3477cef21728c7ddec51ce817
utkarshpathak44 <utkarsh.pathak@remotestate.com>
2025-02-03 17:42:03 +0530

    added profile menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const addButton = document.getElementById("add");
 const closeButton = document.getElementById("close");
 const fixedDiv = document.getElementById("fixedDiv");
 
-fixedDiv.classList.toggle("hidden")
+fixedDiv.classList.toggle("hidden");
 
 addButton.addEventListener("click", () => {
   fixedDiv.classList.toggle("hidden"); // Toggle visibility
@@ -11,3 +11,19 @@ addButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   fixedDiv.classList.add("hidden"); // Hide the div
 });
+
+document.getElementById("dropdownBtn").addEventListener("click", function () {
+  document.getElementById("profileMenu").classList.toggle("hidden");
+});
+
+document.getElementById("closeProfile").addEventListener("click", function () {
+  document.getElementById("profilePage").classList.add("hidden");
+});
+
+function openProfile() {
+  document.getElementById("profilePage").classList.remove("hidden");
+}
+document.getElementById("openProfile").addEventListener("click",openProfile)
+
+
+document.getElementById("openProfilePhone").addEventListener("click",openProfile)
